eac5c6b23f858a15ef8df488fdc4cc0bef13f693
Vincent To <vincent_to@139.com>
2025-01-11 18:34:51 +0800

    Update mihomo-subs-converter: 🔍 revise file 'fix.ts' for removing invalid html elements, update html files and demo links ⚓

--- mihomo-subs-converter/fix.ts
+++ mihomo-subs-converter/fix.ts
@@ -8,10 +8,15 @@ export async function getHandler(request: Request): Promise<Response> {
   if (link) {
     try {
       const response: Response = await fetch(link);
-      const text: string = await response.text();
-      const yamlObj: MihomoTemplateProxiesConfig = YAML.parse(text) as MihomoTemplateProxiesConfig;
+      const originalText: string = await response.text();
+      let processingText: string = originalText;
+      console.log(`Fixing link: ${link}`);
+      // 1. Remove invalid html element <pre> & </pre>.
+      processingText = processingText.replace(/\<\/?pre.*\>/gm, '');
+      console.log(`1. The process of removing invalid html elements '<pre>' & '</pre>' accomplished.`);
+      // 2. Remove SS nodes with the incorrect configuration of 'cipher: ss'.
+      const yamlObj: MihomoTemplateProxiesConfig = YAML.parse(processingText) as MihomoTemplateProxiesConfig;
       if (yamlObj['proxies']) {
-        console.log(`Fixing link: ${link}`);
         const oldProxiesCount : number = yamlObj['proxies'].length;
         const invalidProxies: Array<BaseNode> = yamlObj['proxies'].filter(proxy => (proxy['type'] === 'ss' && !isCipherValidForSSNode(proxy['cipher'])));
         const invalidProxiesName: Array<string> = invalidProxies.map(invalidProxy => invalidProxy['name']);
@@ -19,13 +24,14 @@ export async function getHandler(request: Request): Promise<Response> {
         yamlObj['proxy-groups'].forEach((proxyGroup) => {
           proxyGroup['proxies'] = proxyGroup['proxies'].filter(proxyName => !invalidProxiesName.includes(proxyName));
         });
-        console.log(`Source file has ${oldProxiesCount} proxies, Fixed file has ${yamlObj['proxies'].length} proxies. Removed ${oldProxiesCount - yamlObj['proxies'].length} invalid proxies.`);
-        if (invalidProxiesName.length > 0) console.log(`Removed proxies: ${invalidProxiesName}`);
+        console.log(`2. The process of removing SS nodes with the incorrect configuration of 'cipher: ss' accomplished. Counted proxies: ${oldProxiesCount} => ${yamlObj['proxies'].length}, total ${oldProxiesCount - yamlObj['proxies'].length} proxies removed.`);
+        if (invalidProxiesName.length > 0) console.log(`   Removed proxies: ${invalidProxiesName}`);
+        console.log('OK');
         return new Response(encodeHexUnicode(YAML.stringify(yamlObj)), { status: 200 });
       }
-      throw new TypeError(`Fetched content is NOT a yaml file. We got text below... 🧐\n\n${text}`);
+      else throw new TypeError(`🧐 YamlError: Unable to seek the YAML object 'proxies', original text content listed below:\n\n${originalText}`);
     } catch (error) {
-      return new Response(error as string, { status: 500 });
+      return new Response(`🧐 ${error as string}`, { status: 500 });
     }
   }
   return await fetch(new URL(`./fix.html`, import.meta.url));
