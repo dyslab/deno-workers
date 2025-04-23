6a0597a9b674ca05eb6666f493b5ffccdf249edd
Vincent To <vincent_to@139.com>
2025-04-23 16:24:36 +0800

    Update fix.ts to fix syntax error with '!&lt;str&gt; ' in certain yaml files 🚩

--- mihomo-subs-converter/fix.ts
+++ mihomo-subs-converter/fix.ts
@@ -11,10 +11,13 @@ export async function getHandler(request: Request): Promise<Response> {
       const originalText: string = await response.text();
       let processingText: string = originalText;
       console.log(`Fixing link: ${link}`);
-      // 1. Remove invalid html element <pre> & </pre>.
+      // 1. Remove invalid html tags '<pre>' & '</pre>'.
       processingText = processingText.replace(/\<\/?pre.*\>/gm, '');
-      console.log(`1. The process of removing invalid html elements '<pre>' & '</pre>' accomplished.`);
-      // 2. Remove SS nodes with the incorrect configuration of 'cipher: ss'.
+      console.log(`1. The process of removing invalid html tags '<pre>' & '</pre>' accomplished.`);
+      // 2. Remove invalid yaml string '!&lt;str&gt; '.
+      processingText = processingText.replace(/!&lt;(str)?&gt;\s/gm, '');
+      console.log(`2. The process of removing invalid yaml string '!&lt;str&gt; ' accomplished.`);
+      // 3. Remove SS nodes with the incorrect configuration of 'cipher: ss'.
       const yamlObj: MihomoTemplateProxiesConfig = YAML.parse(processingText) as MihomoTemplateProxiesConfig;
       if (yamlObj['proxies']) {
         const oldProxiesCount : number = yamlObj['proxies'].length;
@@ -24,7 +27,7 @@ export async function getHandler(request: Request): Promise<Response> {
         yamlObj['proxy-groups'].forEach((proxyGroup) => {
           proxyGroup['proxies'] = proxyGroup['proxies'].filter(proxyName => !invalidProxiesName.includes(proxyName));
         });
-        console.log(`2. The process of removing SS nodes with the incorrect configuration of 'cipher: ss' accomplished. Counted proxies: ${oldProxiesCount} => ${yamlObj['proxies'].length}, total ${oldProxiesCount - yamlObj['proxies'].length} proxies removed.`);
+        console.log(`3. The process of removing SS nodes with the incorrect configuration of 'cipher: ss' accomplished. Counted proxies: ${oldProxiesCount} => ${yamlObj['proxies'].length}, total ${oldProxiesCount - yamlObj['proxies'].length} proxies removed.`);
         if (invalidProxiesName.length > 0) console.log(`   Removed proxies: ${invalidProxiesName}`);
         console.log('OK');
         return new Response(encodeHexUnicode(YAML.stringify(yamlObj)), { status: 200 });
